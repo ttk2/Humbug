ba1a31d2fa02c15a02416715b01edd4499da37ea
Arthur-G <arthur.c.goldberg@gmail.com>
2013-08-06 19:55:51 +0100

    Update Humbug.java

--- src/main/java/com/untamedears/humbug/Humbug.java
+++ src/main/java/com/untamedears/humbug/Humbug.java
@@ -83,6 +83,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.Server;
 
 import com.untamedears.humbug.annotations.BahHumbug;
 import com.untamedears.humbug.annotations.BahHumbugs;
@@ -1184,6 +1185,44 @@ public class Humbug extends JavaPlugin implements Listener {
       }
     }
   }
+
+
+  //=================================================
+  // Nerfs Strength Potions to Pre-1.6 Levels
+
+
+  @EventHandler
+  public void onPlayerDamage(EntityDamageByEntityEvent event) {
+    if (!(event.getDamager() instanceof Player)) {
+        return;
+    }
+    Player player = (Player)event.getDamager();
+    if (player.hasPotionEffect(PotionEffectType.INCREASE_DAMAGE))
+    {
+      for (PotionEffect Effect : player.getActivePotionEffects())
+      {
+         if (Effect.getType().equals(PotionEffectType.INCREASE_DAMAGE))
+        {
+            double DamagePercentage = (Effect.getAmplifier() + 1) * 1.3D + 1.0D;
+            int NewDamage;
+            int NewDamage;
+            if (event.getDamage() / DamagePercentage <= 1.0D)
+            {
+              NewDamage = (Effect.getAmplifier() + 1) * 3 + 1;
+            }
+            else
+            {
+              NewDamage = (int)(event.getDamage() / DamagePercentage) + (Effect.getAmplifier() + 1) * 3;
+            }
+            event.setDamage(NewDamage);
+            break;
+          
+        }
+      }
+    }
+  }
+
+
 
   //=================================================
   // Bow shots cause slow debuff
