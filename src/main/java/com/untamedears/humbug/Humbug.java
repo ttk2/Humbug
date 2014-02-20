2ac276d6f2c5475ac76c4761fd07179f480cfe38
TeaJizzle <tom@tomjervis.co.uk>
2014-02-20 19:02:11 +0000

    Add hunger_slowdown option
    Gives players a set amount of saturation every time they lose a point of hunger.
    
    Also adds set saturation to their saturation when they eat food.

--- src/main/java/com/untamedears/humbug/Humbug.java
+++ src/main/java/com/untamedears/humbug/Humbug.java
@@ -1883,6 +1883,15 @@ public class Humbug extends JavaPlugin implements Listener {
   }
 
   // ================================================
+  // Hunger Changes
+  @BahHumbug(opt="hunger_slowdown", type=OptType.Int, def="0")
+  @EventHandler
+  public void onFoodLevelChange(FoodLevelChangeEvent event) {
+	  Player player = (Player) event.getEntity();
+	  player.setSaturation(player.getSaturation() + config_.get("hunger_slowdown").getInt());
+  }
+
+  // ================================================
   // General
 
   public void onLoad()
